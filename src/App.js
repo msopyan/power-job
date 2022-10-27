ccfcbfd5320f1f7d4fe9fd6cfe41e7b97b9bafe2
Faqih Sopyan <faqihsopyan@gmail.com>
2022-10-27 17:30:23 +0700

    update router app.js

--- src/App.js
+++ src/App.js
@@ -9,10 +9,7 @@ import AnnualReport from './pages/annual';
 import Teams from './pages/team';
 import Blogs from './pages/blogs';
 import SignUp from './pages/signup';
-<<<<<<< HEAD
 import SignIn from './pages/signin';
-=======
->>>>>>> update navbar
 
 function App() {
 return (
@@ -26,10 +23,7 @@ return (
 		<Route path='/team' component={Teams} />
 		<Route path='/blogs' component={Blogs} />
 		<Route path='/sign-up' component={SignUp} />
-<<<<<<< HEAD
     <Route path='/sign-in' component={SignIn} />
-=======
->>>>>>> update navbar
 	</Routes>
 	</Router>
 );
