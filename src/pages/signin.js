d3c7044b6d12d28943ceb7a6103ef4a3fd2ee7b4
Faqih Sopyan <faqihsopyan@gmail.com>
2022-10-27 17:09:01 +0700

    update navbar

---
+++ src/pages/signin.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const SignIn = () => {
+return (
+	<div
+	style={{
+		display: 'flex',
+		justifyContent: 'Right',
+		alignItems: 'Right',
+		height: '100vh'
+	}}
+	>
+	<div class="pt-10 pb-28 border-t border-slate-200 sm:flex justify-between text-slate-500 dark:border-slate-200/5">
+        <div class="card">
+            <h1>Sign In</h1>
+        </div>
+	</div>
+</div>
+);
+};
+
+export default SignIn;
