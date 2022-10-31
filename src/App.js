ca0e1e2f69806710b9293e45c3cdba0aa539faf0
Faqih Sopyan <faqihsopyan@gmail.com>
2022-10-31 16:25:04 +0700

    remove all file & add new file

--- src/App.js
+++ src/App.js
@@ -1,32 +1,25 @@
-import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages';
-import About from './pages/about';
-import Events from './pages/events';
-import AnnualReport from './pages/annual';
-import Teams from './pages/team';
-import Blogs from './pages/blogs';
-import SignUp from './pages/signup';
-import SignIn from './pages/signin';
 
 function App() {
-return (
-	<Router>
-	<Navbar />
-	<Routes>
-		<Route path='/' exact component={Home} />
-		<Route path='/about' component={About} />
-		<Route path='/events' component={Events} />
-		<Route path='/annual' component={AnnualReport} />
-		<Route path='/team' component={Teams} />
-		<Route path='/blogs' component={Blogs} />
-		<Route path='/sign-up' component={SignUp} />
-    <Route path='/sign-in' component={SignIn} />
-	</Routes>
-	</Router>
-);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
